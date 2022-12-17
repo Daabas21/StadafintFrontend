93a3d7ba83c678beb483b0f64d037d8d2bf7b2fa
Annika <a.staalhandske@gmail.com>
2022-12-17 22:41:26 +0100

    manadge to uppdate status and cleners in unconfirmed bookings

--- src/components/routes.js
+++ src/components/routes.js
@@ -2,14 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import CleanerView from "./views/CleanerView";
 import CustomerView from "./views/CustomerView";
 import RegisterUser from "./views/RegisterUser";
+import AdminPage from "./views/AdminPage";
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="/cleaner" element={<CleanerView />} />
       <Route path="/customer" element={<CustomerView />} />
-      <Route path="/user" element={<RegisterUser />} />
+      <Route path="/RegisterUser" element={<RegisterUser />} />
+      <Route path="/AdminPage" element={<AdminPage />} />
       <Route path="/login" element={<CustomerView />} />
+      <Route path="/logout" element={<CustomerView />} />
       {/* Add log in component here Hassan */}
     </Routes>
   );
