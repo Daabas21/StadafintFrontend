8eb52895203261528d9784966230db0ecafb7ded
Daabas21 <hassandaabas@hotmail.com>
2022-12-22 00:56:01 +0100

    registration finished

--- src/components/views/RegisterUser.jsx
+++ src/components/views/RegisterUser.jsx
@@ -2,13 +2,14 @@ import * as React from "react";
 import { Box, Typography, TextField, Button, Stack } from "@mui/material";
 import { useState } from "react";
 
+const setToken = (token) => {
+  localStorage.setItem("token", token);
+  localStorage.setItem("lastLoginTime", new Date(Date.now()).getTime);
+}
+
 const RegisterUser = () => {
   const [values, setValues] = useState({
-    name: "",
-    address: "",
-    telnum: "",
-    email: "",
-    password: "",
+    name: "", address: "", telnum: "", email: "", password: "",
   });
 
   const handleChange = (e) => {
@@ -16,8 +17,8 @@ const RegisterUser = () => {
     console.log(e.target.value);
   };
 
-  const handleSubmit = (e) => {
-    fetch("http://localhost:8080/customer", {
+  const handleSubmit = async (e) => {
+    const res = await fetch("http://localhost:8080/auth/register", {
       method: "POST",
       headers: { "Content-type": "application/json" },
       body: JSON.stringify({
@@ -28,14 +29,24 @@ const RegisterUser = () => {
         password: values.password,
       }),
     })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((e) => console.log("Error " + e));
+
+
+    const data = await res.json()
+    const token = data.token
+
+    setToken(token);
+
   };
 
   function handleClick() {
-    handleSubmit();
-    alert("Your account is created ");
+    if (!values.address || !values.name || !values.password || !values.email) {
+      alert("Name, adress, email and password are required!")
+    } else {
+
+      handleSubmit();
+      alert("Your account is created ");
+      setValues({ name: "", address: "", telnum: "", email: "", password: "" })
+    }
   }
 
   return (
@@ -48,69 +59,69 @@ const RegisterUser = () => {
       >
         Register User
       </Typography>
-      <Box sx={{ width: "100%" }}>
-        <form>
-          <Stack direction="column" ml={10} mr={10}>
-            <TextField
-              id="outlined-basic"
-              name="name"
-              label="Name"
-              variant="outlined"
-              value={values.fullname}
-              onChange={handleChange}
-              margin="normal"
-            />
-            <TextField
-              id="outlined-basic"
-              name="address"
-              label="Address"
-              variant="outlined"
-              value={values.address}
-              onChange={handleChange}
-              margin="normal"
-            />
+      <Box width={'100%'}>
+        <Stack direction="column" alignItems={"center"} justifyItems='center'>
+          <TextField
+            id="outlined-basic"
+            name="name"
+            label="Name"
+            variant="outlined"
+            value={values.name}
+            onChange={handleChange}
+            margin="normal"
+            style={{ width: '20%' }}
+          />
+          <TextField
+            id="outlined-basic"
+            name="address"
+            label="Address"
+            variant="outlined"
+            value={values.address}
+            onChange={handleChange}
+            margin="normal"
+            style={{ width: '20%' }}
+          />
 
-            <TextField
-              id="outlined-basic"
-              name="telnum"
-              label="Phone"
-              variant="outlined"
-              value={values.telnum}
-              onChange={handleChange}
-              margin="normal"
-            />
-            <TextField
-              id="outlined-basic"
-              name="email"
-              label="Email"
-              variant="outlined"
-              value={values.email}
-              onChange={handleChange}
-              margin="normal"
-            />
+          <TextField
+            id="outlined-basic"
+            name="telnum"
+            label="Phone"
+            variant="outlined"
+            value={values.telnum}
+            onChange={handleChange}
+            margin="normal"
+            style={{ width: '20%' }}
+          />
+          <TextField
+            id="outlined-basic"
+            name="email"
+            label="Email"
+            variant="outlined"
+            value={values.email}
+            onChange={handleChange}
+            margin="normal"
+            style={{ width: '20%' }}
+          />
 
-            <TextField
-              id="outlined-basic"
-              name="password"
-              label="Password"
-              variant="outlined"
-              type={"password"}
-              value={values.password}
-              onChange={handleChange}
-              margin="normal"
-            />
-          </Stack>
-
+          <TextField
+            id="outlined-basic"
+            name="password"
+            label="Password"
+            variant="outlined"
+            type={"password"}
+            value={values.password}
+            onChange={handleChange}
+            margin="normal"
+            style={{ width: '20%' }}
+          />
           <Button
-            type={"submit"}
             color={"primary"}
             variant={"outlined"}
             onClick={handleClick}
             margin="normal"
-          >
-            Create Account
-          </Button>
-        </form>
+          >Create Account</Button>
+        </Stack>
+
       </Box>
     </div>
   );
