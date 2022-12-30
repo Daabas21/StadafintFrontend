28b3058dc7aa0808066e546661fe623146086e55
Annika <a.staalhandske@gmail.com>
2022-12-30 11:30:49 +0100

    fixed fetch error and cleand up code

--- src/components/views/AdminViews/AdminCustomerCard.js
+++ src/components/views/AdminViews/AdminCustomerCard.js
@@ -1,14 +1,4 @@
 import * as React from "react";
-import {
-  ListItemText,
-  Typography,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  TextField,
-  ListItem,
-} from "@mui/material";
 import CardView from "../../common/CardView";
 
 const AdminCustomerCard = ({
@@ -19,30 +9,6 @@ const AdminCustomerCard = ({
 }) => {
   const usertype = "customer";
 
-  //   const handleChange = (e, c) => {
-  //     c[e.target.name] = e.target.value;
-
-  //     let customerCopy = [...customers];
-
-  //     customerCopy[customerCopy.findIndex((cl) => cl.id === c.id)] = { ...c };
-
-  //     setCustomers(customerCopy);
-  //     console.log(customerCopy);
-  //   };
-
-  //   const handleClick = (customer) => {
-  //     fetch(`http://localhost:8080/admin/customer/${customer.id}`, {
-  //       method: "PUT",
-  //       headers: {
-  //         "Content-type": "application/json",
-  //         Authorization: "Bearer " + localStorage.getItem("token"),
-  //       },
-  //       body: JSON.stringify(customer),
-  //     }).catch((e) => console.log(e));
-
-  //     setBookingView(false);
-  //   };
-
   return (
     <CardView
       user={customer}
@@ -51,68 +17,6 @@ const AdminCustomerCard = ({
       setUsers={setCustomers}
       usertype={usertype}
     />
-    // <Card sx={{ minWidth: 275 }}>
-    //   <CardContent>
-    //     <ListItemText
-    //       primary={
-    //         <Typography variant="h6" style={{ color: "#1976d2" }}>
-    //           ID:{customer.id} - {customer.name.toUpperCase()}
-    //         </Typography>
-    //       }
-    //     />
-    //     <ListItemText
-    //       secondary={
-    //         <Typography style={{ color: "#1976d2" }}>
-    //           Email: {customer.email}
-    //         </Typography>
-    //       }
-    //     />
-    //     <ListItemText
-    //       secondary={
-    //         <Typography style={{ color: "#1976d2" }}>
-    //           Role: {customer.roles}
-    //         </Typography>
-    //       }
-    //     />
-    //     <div
-    //       style={{
-    //         display: "flex",
-    //         flexDirection: "column",
-    //         width: 200,
-    //         gap: 12,
-    //         marginTop: 15,
-    //       }}
-    //     >
-    //       <TextField
-    //         id="outlined-basic"
-    //         variant="outlined"
-    //         label={customer.address}
-    //         defaultValue={customer.address}
-    //         name="address"
-    //         onChange={(e) => handleChange(e, customer)}
-    //       />
-    //       <TextField
-    //         id="outlined-basic"
-    //         variant="outlined"
-    //         label={customer.telnum}
-    //         defaultValue={customer.telnum}
-    //         name="telnum"
-    //         onChange={(e) => handleChange(e, customer)}
-    //       />
-    //     </div>
-    //   </CardContent>
-    //   <CardActions>
-    //     <Button
-    //       sx={{ marginLeft: 1 }}
-    //       type={"submit"}
-    //       color={"primary"}
-    //       variant={"outlined"}
-    //       onClick={() => handleClick(customer)}
-    //     >
-    //       Save
-    //     </Button>
-    //   </CardActions>
-    // </Card>
   );
 };
 
