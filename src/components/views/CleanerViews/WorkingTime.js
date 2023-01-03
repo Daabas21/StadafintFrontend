35d74ca7c8e2b86e782ecf3cd7bfabaab1e1488f
Annika <a.staalhandske@gmail.com>
2023-01-03 10:10:22 +0100

    updated folder structure, added a landing page when admin, cleaner, custumer log in insted of using home page

---
+++ src/components/views/CleanerViews/WorkingTime.js
@@ -0,0 +1,110 @@
+import DatePicker from "react-datepicker";
+import React, { useEffect, useState } from "react";
+import "./WorkingTime.css";
+import BookingCard from "../../common/BookingCard";
+
+import "react-datepicker/dist/react-datepicker.css";
+import { Stack } from "@mui/system";
+import { format } from "date-fns";
+import { Button, Grid, Typography } from "@mui/material";
+
+const WorkingTime = () => {
+  const [dateRange, setDateRange] = useState([null, null]);
+  const [date1, date2] = dateRange;
+
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+
+  const [doneJobs, setDoneJobs] = useState();
+
+  useEffect(() => {
+    if (date1 && date2) {
+      setStartDate(format(date1, "yyyy-MM-dd"));
+      setEndDate(format(date2, "yyyy-MM-dd"));
+    }
+  }, [date1, date2]);
+
+  const searchDoneJobs = () => {
+    if (startDate && endDate) {
+      fetch(
+        `http://localhost:8080/cleaner/1/date?startDate=${startDate}&endDate=${endDate}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((data) =>
+          data.sort((a, b) => Date.parse(a.date) - Date.parse(b.date))
+        )
+        .then((data) => setDoneJobs(data));
+    } else {
+      alert("you must enter start and end date");
+    }
+  };
+
+  const handleCalendarOpen = () => {
+    document.addEventListener(
+      "touchstart",
+      (event) => {
+        event.stopPropagation();
+      },
+      true
+    );
+  };
+
+  const calculateWorkingTime = () => {
+    const arrWorkingTime = doneJobs.map((t) => t.workingTime);
+
+    let sum = arrWorkingTime.reduce((a, b) => a + b);
+
+    alert("Your working time for this period is " + sum + " hours");
+  };
+
+  return (
+    <Stack alignItems={"center"} spacing={2}>
+      <Typography
+        id="title"
+        component="h1"
+        variant="h5"
+        marginTop={2}
+        marginBottom={2}
+      >
+        Check you working hours
+      </Typography>
+      <DatePicker
+        selectsRange={true}
+        startDate={date1}
+        endDate={date2}
+        onChange={(update) => setDateRange(update)}
+        dateFormat="dd-MM-yyyy"
+        withPortal
+        onCalendarOpen={handleCalendarOpen}
+        placeholderText={"Please select dates"}
+      />
+      <Button color={"primary"} variant={"contained"} onClick={searchDoneJobs}>
+        Check
+      </Button>
+      <Grid container spacing={2} wrap={"wrap"} justifyContent={"center"}>
+        {doneJobs
+          ? doneJobs.map((booking) => {
+              return <BookingCard booking={booking} />;
+            })
+          : null}
+      </Grid>
+      {doneJobs ? (
+        <Button
+          color={"primary"}
+          variant={"outlined"}
+          onClick={calculateWorkingTime}
+        >
+          See total
+        </Button>
+      ) : null}
+    </Stack>
+  );
+};
+
+export default WorkingTime;
---
+++ src/components/views/CustomerViews/CustomerBookings.js
@@ -0,0 +1,120 @@
+import { useEffect, useState } from "react";
+import { IconButton, Typography } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { DataGrid } from "@mui/x-data-grid";
+
+import BookingHistory from "./BookingHistory";
+
+const CustomerBookings = () => {
+  const [bookingList, setBookingList] = useState([]);
+  const [customerId, setCustomerId] = useState(0);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/customer/1", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCustomerId(data.id);
+      });
+    getData().then((r) => r);
+  }, [customerId]);
+
+  const getData = async () => {
+    let res = await fetch(
+      `http://localhost:8080/customer/${customerId}/booking`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      }
+    );
+    let data = await res.json();
+
+    setBookingList(data);
+  };
+
+  const handleDelete = async (id) => {
+    const choice = window.confirm(
+      "Are you sure you want to delete this booking? This action can not be undone."
+    );
+    if (choice) {
+      await fetch(`http://localhost:8080/customer/booking/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      await getData();
+    }
+  };
+
+  const onClick = (e, row) => {
+    e.stopPropagation();
+    handleDelete(row.bookingId);
+  };
+
+  const columns = [
+    { field: "address", headerName: "Address", width: 200 },
+    { field: "date", headerName: "Date", width: 200 },
+    { field: "time", headerName: "Time", width: 200 },
+    { field: "service", headerName: "Service", width: 180 },
+    { field: "status", headerName: "Status", width: 200 },
+    {
+      field: "actions",
+      headerName: "Unbook",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <IconButton
+            variant="outlined"
+            color="error"
+            onClick={(e) => onClick(e, params.row)}
+          >
+            <CloseIcon />
+          </IconButton>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div
+      style={{
+        margin: 20,
+        textAlign: "center",
+      }}
+    >
+      <Typography
+        id="title"
+        component="h1"
+        variant="h5"
+        marginTop={2}
+        marginBottom={2}
+      >
+        Bookings
+      </Typography>
+
+      <div style={{ height: 400, width: "auto" }}>
+        <DataGrid
+          rows={bookingList.filter((booking) => booking.status !== "Performed")}
+          getRowId={(row) => row?.bookingId}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+        />
+      </div>
+
+      <BookingHistory bookingList={bookingList} />
+    </div>
+  );
+};
+
+export default CustomerBookings;
